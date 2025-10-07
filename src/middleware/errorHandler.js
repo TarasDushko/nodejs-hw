bdf81e462a24c5b4c60f8678edce838ce16ac8da
Taras <tarasdushko@gmail.com>
2025-10-07 13:55:58 +0200

    fix nc

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,13 @@
-export const errorHandler = (err, req, res, next) => {
-  console.error(err.message);
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (error, req, res, next) => {
+  if (error instanceof HttpError) {
+    return res.status(error.status).json({
+      message: error.message || error.name,
+    });
+  }
+  console.error(error);
   res.status(500).json({
-    message: err.message,
+    message: error.message,
   });
 };
