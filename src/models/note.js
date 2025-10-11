3b1f4569d55b1f3ea9152679584735cfda8f4d6f
Taras <tarasdushko@gmail.com>
2025-10-11 09:01:22 +0200

    fix server note

--- src/models/note.js
+++ src/models/note.js
@@ -6,15 +6,16 @@ const noteSchema = new Schema(
     title: {
       type: String,
       required: true,
+      trim: true,
     },
     content: {
       type: String,
       required: false,
+      trim: true,
       default: '',
     },
     tag: {
       type: String,
-      required: true,
       enum: [
         'Work',
         'Personal',
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
+import helmet from 'helmet';
 import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
@@ -9,15 +9,26 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
+
 const PORT = process.env.PORT ?? 3030;
-await connectMongoDB();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(logger); //  Логер першим — бачить усі запити
 
-app.use(logger); // 1. Логер першим — бачить усі запити
+app.use(
+  express.json({
+    limit: '100kb',
+    type: ['application/json', 'application/vnd.api+json'],
+  }),
+);
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(notesRoutes);
+
 app.use(notFoundHandler);
+
 app.use(errorHandler);
 
 await connectMongoDB();
@@ -25,52 +36,3 @@ await connectMongoDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-app.use(express.json());
-app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
-
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: err.message,
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-app.use(notesRoutes);
