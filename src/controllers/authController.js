361f229cd3338dd6163b8f89c46b07407a74a81b
Taras <tarasdushko@gmail.com>
2025-10-14 11:08:32 +0200

    add services auth

---
+++ src/controllers/authController.js
@@ -0,0 +1,26 @@
+// src/controllers/authController.js
+
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+
+export const registerUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return next(createHttpError(400, 'Email in use'));
+  }
+
+  // Хешуємо пароль
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Створюємо користувача
+  const newUser = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  // Відправляємо дані користувача (без пароля) у відповіді
+  res.status(201).json(newUser);
+};
