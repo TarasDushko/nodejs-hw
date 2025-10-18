b99cc3dd346eae0997c17cae5f057afb192fa717
Taras <tarasdushko@gmail.com>
2025-10-18 18:26:45 +0200

    add email password env

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -6,9 +6,23 @@ export const registerUserSchema = {
     password: Joi.string().min(8).required(),
   }),
 };
+
 export const loginUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
+  }),
+};
+
+export const requestResetEmailSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+  }),
+};
+
+export const resetPasswordSchema = {
+  [Segments.BODY]: Joi.object({
+    password: Joi.string().required(),
+    token: Joi.string().required(),
   }),
 };
