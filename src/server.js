78c59a12b6ebd1d1e2667d587c6090041ef9f795
Taras <tarasdushko@gmail.com>
2025-10-25 10:34:46 +0200

    fix server sendM user

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3030;
 
 app.use(logger); //  Логер першим — бачить усі запити
 
